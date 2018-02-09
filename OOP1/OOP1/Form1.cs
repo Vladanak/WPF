b787e9b839bd97ae59674bff4e2d1108bf71ede2
Vladanak <vladmyr89@gmail.com>
2018-02-09 23:45:14 +0300

    WPF

---
+++ OOP1/OOP1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ">";
+            if (textBox1.Text.Length > textBox2.Text.Length)
+                textBox3.Text = "True";
+            if (textBox1.Text.Length < textBox2.Text.Length)
+                textBox3.Text = "False";
+            if (textBox1.Text.Length == textBox2.Text.Length)
+                textBox3.Text = "False";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "<";
+            if (textBox1.Text.Length > textBox2.Text.Length)
+                textBox3.Text = "False";
+            if (textBox1.Text.Length < textBox2.Text.Length)
+                textBox3.Text = "True";
+            if (textBox1.Text.Length == textBox2.Text.Length)
+                textBox3.Text = "False";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "==";
+            if (textBox1.Text.Length > textBox2.Text.Length)
+                textBox3.Text = "False";
+            if (textBox1.Text.Length < textBox2.Text.Length)
+                textBox3.Text = "False";
+            if (textBox1.Text.Length == textBox2.Text.Length)
+                textBox3.Text = "True";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "!=";
+            if (textBox1.Text.Length > textBox2.Text.Length)
+                textBox3.Text = "True";
+            if (textBox1.Text.Length < textBox2.Text.Length)
+                textBox3.Text = "True";
+            if (textBox1.Text.Length == textBox2.Text.Length)
+                textBox3.Text = "False";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "<<";
+            if (textBox1.Text.Length > textBox2.Text.Length)
+                textBox3.Text = "True";
+            if (textBox1.Text.Length < textBox2.Text.Length)
+                textBox3.Text = "False";
+            if (textBox1.Text.Length == textBox2.Text.Length)
+                textBox3.Text = "False";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = ">>";
+            if (textBox1.Text.Length > textBox2.Text.Length)
+                textBox3.Text = "True";
+            if (textBox1.Text.Length < textBox2.Text.Length)
+                textBox3.Text = "False";
+            if (textBox1.Text.Length == textBox2.Text.Length)
+                textBox3.Text = "False";
+        }
+    }
+}
